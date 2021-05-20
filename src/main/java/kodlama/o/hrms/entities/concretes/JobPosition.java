1f7ce0c8a6aa6da7c69db0b6531716a749f677a5
Nurhayat <69644512+nurhayatketenci@users.noreply.github.com>
2021-05-20 18:22:59 +0300

    commit1

--- src/main/java/kodlama/o/hrms/entities/concretes/JobPosition.java
+++ src/main/java/kodlama/o/hrms/entities/concretes/JobPosition.java
@@ -16,7 +16,7 @@ public class JobPosition {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private int  id;
 
 
     @Column(name = "position")
